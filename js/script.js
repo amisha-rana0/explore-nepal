2175b9633dda9df4fe86ba3d25449e8531d5e377
amisha-rana0 <amisharana2062@gmail.com>
2025-07-14 02:33:27 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+	alert("Hello, Professor Ozaydin. Welcome to Explore Nepal – Discover places, adventures, and local food!");
+};
+
+function myFunction1() {
+	document.getElementById("suggest").innerHTML = "🏞️ Pokhara - Enjoy boating on Phewa Lake, paragliding from Sarangkot, and cave exploration while savoring trout dishes, Thakali sets, and momos by the lakeside.";
+};
+
+function myFunction2() {
+	document.getElementById("suggest").innerHTML = "🛕 Kathmandu - Explore ancient temples and bustling markets, dive into Newari culture, and indulge in traditional foods like yomari, chatamari, and sel roti.";
+};
+
+function myFunction3() {
+	document.getElementById("suggest").innerHTML = "🏔️ Mustang - Trek through dramatic Himalayan landscapes, visit sky caves and monasteries, and warm up with dhido, yak butter tea, and Mustangi specialties.";
+};
